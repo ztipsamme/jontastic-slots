bb3096845dad08e56dc56aa2c291c67ba6b58e44
Ca-Ad <65218058+calleballe1@users.noreply.github.com>
2023-02-16 14:29:01 +0100

    setup config

--- src/components/CitiesList.vue
+++ src/components/CitiesList.vue
@@ -1,18 +1,18 @@
 <script>
-  export default {
-    created() {
-      fetch('https://avancera.app/cities/')
-        .then((response) => response.json())
-        .then((cities) => {
-          this.cities = cities
-        })
-    },
-    data() {
-      return {
-        cities: null
-      }
-    }
-  }
+export default {
+  created() {
+    fetch("https://avancera.app/cities/")
+      .then((response) => response.json())
+      .then((cities) => {
+        this.cities = cities;
+      });
+  },
+  data() {
+    return {
+      cities: null,
+    };
+  },
+};
 </script>
 
 <template>
