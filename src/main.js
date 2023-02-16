5918f2ba45935d1e603f78169cc641e56bec88de
Ca-Ad <65218058+calleballe1@users.noreply.github.com>
2023-02-16 15:34:28 +0100

    File Structure / Templating

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/router'
+import router from './router/router.js'
 import { createPinia } from 'pinia'
 
 createApp(App).use(router).use(createPinia()).mount('#app')
