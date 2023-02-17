9478b02203cee42e7144649f5a0179df8392beaf
Moa Englander <moaenglander@gmail.com>
2023-02-17 09:59:40 +0100

    Uppdaterade store med köpfunktioner

--- src/stores/tokenStore.js
+++ src/stores/tokenStore.js
@@ -2,7 +2,15 @@ import { defineStore } from 'pinia'
 export const useTokenStore = defineStore('tokens', {
   strict: true,
   state: () => ({
-    tokens: 0,
+    tokens: 100, // börja med 100
+    bestWin: 0, // börja med noll vinster
+    enhancements: [], // börja med tom lista av köpfunktioner
+    enhancementTypes: [
+      // vilka typer av köpfunktioner
+      { name: 'New Skin', cost: 50 },
+      { name: 'Extra Spin', cost: 100 },
+      { name: 'Extra Row', cost: 150 },
+    ],
   }),
   getters: {
     getTokensPlusOne: (state) => state.tokens + 1,
@@ -13,6 +21,9 @@ export const useTokenStore = defineStore('tokens', {
     },
   },
 })
+
+// lägga till action för purchase enhancement
+// lägga till för att uppdatera best win och på något sätt ladda sparad data
 
 /*
 // För att använda i ViewComponent:
