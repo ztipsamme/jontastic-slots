f50a16364e1324c391015d3becf62cbc28f766fe
Ca-Ad <65218058+calleballe1@users.noreply.github.com>
2023-02-20 20:33:06 +0100

    beforeMerge

--- src/stores/shop.js
+++ src/stores/shop.js
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia"
-export const useBetStore = defineStore("tokens", {
+export const useShopStore = defineStore("shop", {
   strict: true,
   state: () => {
     return {
