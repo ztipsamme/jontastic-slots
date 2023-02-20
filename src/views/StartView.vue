42bb7fd5c6fcab2163b011eb9a12e3fff244aa9b
IsacZaf <isaczafar@gmail.com>
2023-02-20 15:12:57 +0100

    Symbol popup tillagt. Även justerat StartView lite

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,29 +1,62 @@
 <template>
   <h1>Start View</h1>
   <slotgroup />
+  <i class="info-icon" @click="showSymbolPopup = true" />
+  <SymbolPopup
+    v-if="showSymbolPopup"
+    :symbols="symbols"
+    @close="showSymbolPopup = false"
+  />
 </template>
 
 <script>
   import slotgroup from "../components/slotGroupComp.vue"
+  import SymbolPopup from "../components/SymbolValue.vue"
   import { useTokenStore } from "../stores/tokenStore"
+
   export default {
+    components: {
+      slotgroup,
+      SymbolPopup,
+    },
     setup() {
       const tokenStore = useTokenStore()
       return { tokenStore }
     },
-    components: {
-      slotgroup: slotgroup,
-    },
     data() {
-      return {}
+      return {
+        showSymbolPopup: false,
+      }
     },
     computed: {
-      t() {
-        return this.tokenStore
+      symbols() {
+        return this.tokenStore.symbols
       },
     },
-    methods: {},
   }
 </script>
 
-<style></style>
+<style scoped>
+  .info-icon {
+    display: inline-block;
+    position: relative;
+    width: 32px;
+    height: 32px;
+    border: 1px solid #f00;
+    border-radius: 50%;
+    background-color: #fff;
+    cursor: pointer;
+  }
+  .info-icon::before {
+    content: "i";
+    display: block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    font-style: normal;
+    font-size: 30px;
+    font-weight: bold;
+  }
+</style>
