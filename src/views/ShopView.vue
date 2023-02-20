cdfd66743b9663b1a52f36746ad281a47b3942ca
Ca-Ad <65218058+calleballe1@users.noreply.github.com>
2023-02-20 15:55:22 +0100

    storeFix-Shopview

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,35 +1,37 @@
 <script>
-  import { useTokenStore } from "../stores/tokenStore.js"
+import { useTokenStore } from "../stores/tokenStore.js";
 
-  // Importera tokenStore
-  export default {
-    computed: {
-      tokens() {
-        return useTokenStore().tokens // Hämta tokens
-      },
-      bonusTypes() {
-        return useTokenStore().bonusTypes // Hämta typ av köpfunktioner
-      },
+// Importera tokenStore
+export default {
+  setup() {
+    const tokenStore = useTokenStore();
+    return { tokenStore };
+  },
+  computed: {
+    tokens() {
+      return this.tokenStore.tokens; // Hämta tokens
     },
-
-    methods: {
-      buyBonus(name) {
-        const tokenStore = useTokenStore()
-
-        // Sök genom tokenStore och hitta matchande namn som klickats på
-        const bonus = tokenStore.bonusTypes.find((type) => type.name === name)
-        // Kolla om du har nog med tokens som räcker för cost
-        if (bonus && tokenStore.tokens >= bonus.cost) {
-          // Lägg in i din lista med bonusar och dra av kostnad
-          tokenStore.bonusList.push(bonus)
-          tokenStore.tokens -= bonus.cost
-          console.log("Köpte: " + bonus.name)
-        } else {
-          console.log("Du saknar tokens")
-        }
-      },
+    bonusTypes() {
+      return this.tokenStore.bonusTypes; // Hämta typ av köpfunktioner
     },
-  }
+  },
+
+  methods: {
+    buyBonus(name) {
+      // Sök genom tokenStore och hitta matchande namn som klickats på
+      const bonus = this.tokenStore.bonusTypes.find((type) => type.name === name);
+      // Kolla om du har nog med tokens som räcker för cost
+      if (bonus && this.tokenStore.tokens >= bonus.cost) {
+        // Lägg in i din lista med bonusar och dra av kostnad
+        this.tokenStore.bonusList.push(bonus);
+        this.tokenStore.tokens -= bonus.cost;
+        console.log("Köpte: " + bonus.name);
+      } else {
+        console.log("Du saknar tokens");
+      }
+    },
+  },
+};
 </script>
 
 <template>
@@ -46,12 +48,8 @@
       <button class="bonus" @click="buyBonus('Extra Row')">Extra Row</button>
     </div>
     <h2 class="first-heading">Teman</h2>
-    <button class="night-theme" @click="buyBonus('Night Theme')">
-      Night Theme
-    </button>
-    <button class="forest-theme" @click="buyBonus('Forest Theme')">
-      Forest Theme
-    </button>
+    <button class="night-theme" @click="buyBonus('Night Theme')">Night Theme</button>
+    <button class="forest-theme" @click="buyBonus('Forest Theme')">Forest Theme</button>
     <button class="cat-theme" @click="($event) => buyBonus('Cat Theme')">
       Cat Theme
     </button>
