31e4bc11cfdb97ab7cc76d0c3b6b823cad5ce1ef
ztipsamme <e.spitz@hotmail.com>
2023-02-22 14:04:47 +0100

    Pop-up fixes

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,18 +1,15 @@
 <template>
   <h1>Start View</h1>
   <slotgroup />
-  <PopUp />
 </template>
 
 <script>
   import slotgroup from "../components/slotGroupComp.vue"
-  import PopUp from "../components/PopUp.vue"
   import { useTokenStore } from "../stores/tokenStore"
 
   export default {
     components: {
       slotgroup,
-      PopUp,
     },
     setup() {
       const tokenStore = useTokenStore()
