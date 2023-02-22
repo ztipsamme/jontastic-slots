347662521ff451ee6989f2ee0e882fc64074081d
Ca-Ad <65218058+calleballe1@users.noreply.github.com>
2023-02-22 10:27:24 +0100

    responsivaSnurror

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import App from "./App.vue"
 import router from "./router/router.js"
 import { createPinia } from "pinia"
 import "../assets/main.scss"
+
 createApp(App).use(router).use(createPinia()).mount("#app")
