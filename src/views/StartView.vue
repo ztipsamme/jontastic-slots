cf81bccb7558f36bb484510432aa9b7af754d792
ztipsamme <e.spitz@hotmail.com>
2023-02-22 19:27:57 +0100

    Kom igång för Hanna :)

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,15 +1,18 @@
 <template>
   <h1>Start View</h1>
   <slotgroup />
+  <betting />
 </template>
 
 <script>
   import slotgroup from "../components/SlotMachineComp.vue"
+  import betting from "../components/bettingComp.vue"
   import { useTokenStore } from "../stores/tokenStore"
 
   export default {
     components: {
       slotgroup,
+      betting,
     },
     setup() {
       const tokenStore = useTokenStore()
