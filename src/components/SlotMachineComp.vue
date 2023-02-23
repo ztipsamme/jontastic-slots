bdb259511b502058bb6dd1000042833bb1800396
Moa Englander <moaenglander@gmail.com>
2023-02-23 11:51:06 +0100

    Om du äger Extra Row så får du 4 rows, måste justeras så man kan aktivera den

--- src/components/SlotMachineComp.vue
+++ src/components/SlotMachineComp.vue
@@ -8,15 +8,19 @@
     },
     emits: { stop: null },
     created() {
+      var hasBonus = this.tokens.bonusTypes.find((i) => i.name === "Extra Row")
+      if (hasBonus.owned === true) {
+        this.reels = 4
+      } else {
+        this.reels = 3
+      }
+      console.log(hasBonus)
+
       this.spinnerArr = new Array(this.reels)
         .fill(null)
         .map(() => this.generateSpinner())
-      /*       this.spinnerArr = [
-        this.generateSpinner(),
-        this.generateSpinner(),
-        this.generateSpinner(),
-      ] */
     },
+
     setup() {
       const tokens = useTokenStore()
       const theme = useThemeStore()
@@ -31,9 +35,10 @@
         winner: false,
         startGame: false,
         spinnerArr: [],
-        reels: 4,
+        reels: 3,
       }
     },
+
     watch: {
       reels() {
         this.spinnerArr = new Array(this.reels)
@@ -46,6 +51,7 @@
         return this.tokens.tokens.sum
       },
     },
+
     methods: {
       generateSpinner() {
         let arr = []
@@ -64,56 +70,56 @@
         window.arr = arr
         return arr
       },
+    },
 
-      getNumbers() {
-        // this.spinnerArr = new Array(3).fill(this.generateSpinner())
-        let num = []
-        let n = []
+    getNumbers() {
+      let num = []
+      let n = []
 
-        this.spinnerArr.forEach((e, i) => {
+      this.spinnerArr.forEach((e, i) => {
+        n[i] = Math.floor(Math.random() * this.count)
+        num[i] = e[n[i]]
+      })
+
+      if (num[0] == num[1]) {
+        for (let i = 2; i < this.spinnerArr.length; i++) {
+          n[i] = this.spinnerArr[i].indexOf(num[0])
+          num[i] = this.spinnerArr[i][n[i]]
+        }
+      } else {
+        for (let i = 2; i < this.spinnerArr.length; i++) {
           n[i] = Math.floor(Math.random() * this.count)
-          num[i] = e[n[i]]
-        })
-
-        if (num[0] == num[1]) {
-          for (let i = 2; i < this.spinnerArr.length; i++) {
-            n[i] = this.spinnerArr[i].indexOf(num[0])
-            num[i] = this.spinnerArr[i][n[i]]
-          }
-        } else {
-          for (let i = 2; i < this.spinnerArr.length; i++) {
-            n[i] = Math.floor(Math.random() * this.count)
-            num[i] = this.spinnerArr[i][n[i]]
+          num[i] = this.spinnerArr[i][n[i]]
+        }
+      }
+      if (this.reels == 4) {
+        let same = false
+        for (let n in num) {
+          let i = Number(n)
+          if (num.filter((e) => e == num[i]).length > 2) {
+            same = num[i]
+            break
           }
         }
-        if (this.reels == 4) {
-          let same = false
-          for (let n in num) {
-            let i = Number(n)
-            if (num.filter((e) => e == num[i]).length > 2) {
-              same = num[i]
-              break
+        if (same && !num.every((e) => e == same)) {
+          let i = num.findIndex((e) => e != same)
+          let arr = []
+          this.spinnerArr[i].forEach((e, i) => {
+            if (e == same) {
+              arr.push(i)
             }
-          }
-          if (same && !num.every((e) => e == same)) {
-            let i = num.findIndex((e) => e != same)
-            let arr = []
-            this.spinnerArr[i].forEach((e, i) => {
-              if (e == same) {
-                arr.push(i)
-              }
-            })
-            n[i] = arr[Math.floor(Math.random() * arr.length)]
-            num[i] = this.spinnerArr[i][n[i]]
-          }
+          })
+          n[i] = arr[Math.floor(Math.random() * arr.length)]
+          num[i] = this.spinnerArr[i][n[i]]
         }
+      }
 
-        // Sortera så att stösrt sannorlikhet kommerförsts
+      // Sortera så att stösrt sannorlikhet kommerförsts
 
-        /*   n[2] = Math.floor(Math.random() * this.count)
+      /*   n[2] = Math.floor(Math.random() * this.count)
           num[2] = this.spinnerArr[2][n[2]] */
 
-        /*      num[2] =
+      /*      num[2] =
             num[0] == num[1] ? num[0] : Math.floor(Math.random() * this.count)
 
           if (!num.every((e) => e == num[0])) {
@@ -122,45 +128,44 @@
             }
           } */
 
-        return { num: num, n: n }
-      },
-      checkNumbers() {
-        let val = this.getNumbers()
-        this.num = val.num
-        this.n = val.n
+      return { num: num, n: n }
+    },
+    checkNumbers() {
+      let val = this.getNumbers()
+      this.num = val.num
+      this.n = val.n
 
-        return this.n
-      },
-      done() {
-        this.startGame = false
+      return this.n
+    },
+    done() {
+      this.startGame = false
 
-        if (this.num.every((e) => e == this.num[0])) {
-          this.winner = true
-          this.tokens.tokens.sum = this.tokens.tokens.sum + 100
-          console.log("Yay, you won 100 toekns! =D")
-        } else {
-          this.winner = false
-          console.log("Haha, loser. :P")
-        }
-      },
-      gameStart() {
-        if (this.startGame) {
-          return
-        }
-        this.startGame = true
-
-        this.num = [1, 2, 3, 4, 5, 3, 6, 9]
-        if (this.tokens.tokens.sum - 5 < 0) {
-          alert("GameOver")
-          this.tokens.tokens.sum = this.tokens.tokens.startValue
-          return
-        }
+      if (this.num.every((e) => e == this.num[0])) {
+        this.winner = true
+        this.tokens.tokens.sum = this.tokens.tokens.sum + 100
+        console.log("Yay, you won 100 toekns! =D")
+      } else {
         this.winner = false
-        this.tokens.tokens.sum -= 5
-        this.checkNumbers()
+        console.log("Haha, loser. :P")
+      }
+    },
+    gameStart() {
+      if (this.startGame) {
+        return
+      }
+      this.startGame = true
 
-        this.$refs.child.start(this.n)
-      },
+      this.num = [1, 2, 3, 4, 5, 3, 6, 9]
+      if (this.tokens.tokens.sum - 5 < 0) {
+        alert("GameOver")
+        this.tokens.tokens.sum = this.tokens.tokens.startValue
+        return
+      }
+      this.winner = false
+      this.tokens.tokens.sum -= 5
+      this.checkNumbers()
+
+      this.$refs.child.start(this.n)
     },
   }
 </script>
@@ -168,9 +173,6 @@
 <template>
   <div class="main-mashine cont">
     <div class="row">
-      <div class="col">
-        <h1 v-if="winner">WOOOHKOOOO</h1>
-      </div>
       <div class="col">
         <button
           v-if="tokens.isThemeOwned('cattheme')"
