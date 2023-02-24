8e1466b944321d2659552ac099257f249b26390d
Moa Englander <moaenglander@gmail.com>
2023-02-24 14:19:35 +0100

    Information till instruktioner, shop, trygghet

--- src/components/pop-ups/gameInstructions.vue
+++ src/components/pop-ups/gameInstructions.vue
@@ -1,3 +1,35 @@
 <template>
-  <h1>Hjälp</h1>
+  <div class="container">
+    <h1>Instruktioner</h1>
+    <p class="first">
+      Spela och vinn stora priser medan du har kul och spenderar dina virtuella
+      tokens. Du kan satsa på dina egna villkor genom att välja antalet tokens
+      du vill spendera på varje snurr. Dessutom kan du anpassa din
+      spelupplevelse genom att köpa teman som passar din personlighet. För att
+      göra spelet ännu mer spännande finns det två bonusfunktioner, Extra Spin
+      som ger dig en extra chans att snurra hjulet och Extra Row som lägger till
+      en extra rad för att öka dina chanser att vinna. Du kan vinna bonusar
+      eller köpa dom för dina tokens i vår butik.
+    </p>
+    <p class="second">
+      För att börja spela trycker du på "Spela"-knappen. Du kan börja med att
+      satsa 5 tokens eller välja din insats med betting-knappen. Får du tre i
+      rad så vinner du 100 tokens!
+    </p>
+  </div>
 </template>
+
+<style>
+  h1 {
+    font-size: 20px;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  p {
+    text-align: center;
+  }
+  .container {
+    margin-bottom: 20px;
+  }
+</style>
--- src/components/pop-ups/shopPopUp.vue
+++ src/components/pop-ups/shopPopUp.vue
@@ -2,14 +2,26 @@
   <div class="container">
     <h1>Välkommen till din butik!</h1>
     <p>
-      Här kan du använda dina tokens för att köpa bonusar som bättrar dina
-      vinstchanser eller ett tema som passar dig. Du behöver bara köpa ett tema
-      en gång och kan byta bland dina favoriter när du känner för det.
+      Här kan du använda dina tokens för att förhöja din spelupplevelse med
+      olika funktioner som går att köpa. Vi erbjuder teman som du kan köpa för
+      att personifiera din spelupplevelse. När du har köpt ett tema kan du
+      använda det när du vill utan någon extra kostnad. Om du vill öka dina
+      vinstchanser, så kan du köpa några av våra bonusar. "Extra Row" ger dig
+      ytterligare en rad i maskinen och "Extra Row" tillåter dig att få en extra
+      chans att snurra på hjulet. Du kan köpa dessa bonusar när du vill, och när
+      du har gjort det kommer de finnas tillgängliga i din väska.
+    </p>
+    <p>
+      För att aktivera dina köpta föremål behöver du bara klicka på dom i din
+      väska.
     </p>
   </div>
 </template>
 
 <style>
+  .container {
+    margin-bottom: 20px;
+  }
   h1 {
     font-size: 20px;
     text-align: center;
