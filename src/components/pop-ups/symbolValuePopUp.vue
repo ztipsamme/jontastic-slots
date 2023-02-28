b053b402e6ce85648ef7ce374fba435b04de22d4
Moa Englander <moaenglander@gmail.com>
2023-02-28 10:56:48 +0100

    Symbol values uppdateras baserat på tema

--- src/components/pop-ups/symbolValuePopUp.vue
+++ src/components/pop-ups/symbolValuePopUp.vue
@@ -1,25 +1,43 @@
 <template>
   <h2 class="popup-title">Symbol Values</h2>
-  <div class="symbol-values">
-    <div
-      v-for="symbolValue in symbolValues"
-      :key="symbolValue.symbol"
-      class="symbol-value"
-    >
-      <img :src="symbolValue.imageUrl" class="symbol-image" />
-      <span class="symbol-name">{{ symbolValue.symbol }}</span>
-      <span class="symbol-points">{{ symbolValue.value }}</span>
-    </div>
+  <div class="symbols">
+    <img v-for="image in icons" :src="image" :key="image" class="symbol" />
   </div>
 </template>
 
 <script>
+  import { useThemeStore } from "../../stores/themes.js"
+
   export default {
-    props: {
-      symbolValues: {
-        type: Array,
-        required: true,
-      },
+    data() {
+      return {
+        currentTheme: "default",
+      }
+    },
+
+    setup() {
+      const theme = useThemeStore()
+      const icons = theme.icons
+      console.log(theme.icons)
+
+      return { theme, icons }
     },
   }
 </script>
+
+<style lang="scss">
+  .popup-title {
+    text-align: center;
+  }
+
+  .symbols {
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+  }
+
+  .symbol {
+    margin-left: 20px;
+    width: 40px;
+  }
+</style>
