21f5ea0d06763ad0bc26107b0aec93299313e948
Moa Englander <moaenglander@gmail.com>
2023-02-28 14:44:57 +0100

    la till eld i themes

--- src/components/themes/index.js
+++ src/components/themes/index.js
@@ -1,10 +1,12 @@
 import _default from "./Default"
 import CatTheme from "./CatTheme"
 import ForestTheme from "./ForestTheme"
+import EldTheme from "./EldTheme"
 const Themes = {
   diamant: _default,
   katt: CatTheme,
   skog: ForestTheme,
+  eld: EldTheme,
 }
 console.log(Themes)
 export default Themes
